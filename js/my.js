ac55bbaa73aa1c7a4c8a64d62fcf8704ddc297e0
365332699 <365332699@qq.com>
2018-01-16 23:57:42 +0800

    我的个人站
    1111

---
+++ js/my.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$(function() {
+    $('#dowebok').fullpage({
+        verticalCentered: true, //内容垂直居中
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90','#f60'],//设置每一屏的颜色 
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],//设置跳转链接
+        navigation: true,//右边角点导航
+        menu: '#menu',//来设置顶部导航
+        resize: true,//窗口缩放事件触发
+        continuousVertical: true,
+    });
+	
+	
+	
+	
+	
+	
+});
+
+
+
